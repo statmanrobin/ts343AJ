0547456dec2cb459bd4c841865d4211443ff1fc7
Robin Lock <rlock@stlawu.edu>
2021-11-23 13:18:56 -0500

    Fixing polychar and showfit to roxyenize documentation

--- R/polyChar.R
+++ R/polyChar.R
@@ -1,10 +1,25 @@
-# polyChar for ARIMA model in STAT-343
-# Function that takes a ARIMA time series model and returns a list characteristic polynomials
 
-library(PolynomF)
+library(PolynomF)    #Gives an error if PolynomF is not installed?
+
+#' Show characteristic polynomials from an Arima model
+#' 
+#' @description The polyChar function takes an ARIMA model and produces a list of characteristic polynomials for the fitted model. The elements in the list are of class “polynomial”. The terms are auto-regressive (ar), seasonal auto-regressive (sar), regular differences (diff), seasonal differences (s diff), moving average (ma), seasonal moving average (sma), and the constant term.
+#' 
+#' @param model  an object of class “arima”. For example, model produced by the Arima() function
+#' 
+#' @details polyChar extracts the coefficients from the ARIMA output and creates a vector of 1 and the coefficient(s) of each term (Example: ma = c(1, coef, coef2)). It then places each vector into a list (finaList). Then using the polynom function from the PolynomF package, polyChar then takes each vector from the list and uses each element within the vector to create a polynomial with ascending degree by the index. Then, an empty polynomial list is created (polyList). The polynomials are assigned an index, named according to their corresponding term in the ARIMA model, and added to polyList. If the term is not present in the model, it defaults to a 1 in the polynomial list. The function then returns the polyList. The list will return a list of 7 elements of the class polynomial.
+#' 
+#' @return A list of characteristic polynomials for a given fitted ARIMA model
+#' 
+#' @export
 
 polyChar = function(model){
   
+# Code below can check for PolynomF pacakge
+#  if (!requireNamespace("PolynomF", quietly = TRUE)) {
+#    stop("Package \"PolynomF\" needed for this function to work. Please install it.",
+#         call. = FALSE)
+#  } 
   
   #The following code extracts the coefficients from the Arima output
   outcoefs = round(model$coef, 3)
@@ -27,7 +42,7 @@ polyChar = function(model){
   d <- model$arma[6]
   D <- model$arma[7]
   
-  # Vectors of coeffiecients are then store in a list
+  # Vectors of coefficients are then store in a list
   finalList = list(ar,ma,d,sar,sma)
   
   polyList = list()
